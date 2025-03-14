1a3903ea2ca9534a66bb09e62fd904a1d0d54a82
Ahn Honghyun <anh2073@naver.com>
2025-03-14 10:38:43 +0900

    feat : createBrowserRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "./screens/Home";
 import About from "./screens/About";
+import Root from "./Root";
 
-function Router() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-export default Router;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+export default router;
