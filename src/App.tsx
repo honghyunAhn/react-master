e131019888250196da21d4094190867cb6ae0077
Ahn Honghyun <anh2073@naver.com>
2025-03-23 21:22:15 +0900

    feat : Home style

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
 
 const GlobalStyle = createGlobalStyle`
-
+  
  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
  html, body, div, span, applet, object, iframe,
  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
@@ -61,6 +61,7 @@ const GlobalStyle = createGlobalStyle`
  }
  a {
    text-decoration:none;
+   color: inherit;
  }
 `;
 
