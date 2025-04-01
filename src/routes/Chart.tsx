035733f5e0f163b7e2d1cbe362821df326bb1e72
Ahn Honghyun <anh2073@naver.com>
2025-04-01 11:34:49 +0900

    feat : Price Chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { useOutletContext } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+interface ChartProps {
+  coinId: string;
+}
 function Chart() {
+  const { coinId } = useOutletContext<ChartProps>();
+  const { isLoading, data } = useQuery({
+    queryKey: ["ohlcv", coinId],
+    queryFn: () => fetchCoinHistory(coinId),
+  });
+  console.log(coinId);
   return <h1>Chart</h1>;
 }
 
