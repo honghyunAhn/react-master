6e17406aadf6d5502fdbbac73be0f3d0edb12130
Ahn Honghyun <anh2073@naver.com>
2025-04-14 11:28:53 +0900

    feat : typeScript Price

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,12 @@
-function Price() {
-  return <h1>Price</h1>;
+interface PriceProps {
+  coinId: string;
+  ath_price: number | undefined;
+  ath_date: string | undefined;
+  percent_change_30m: number | undefined;
+  percent_change_1h: number | undefined;
+  percent_change_24h: number | undefined;
+  percent_change_7d: number | undefined;
+  percent_change_30d: number | undefined;
+  percent_change_1y: number | undefined;
+  percent_from_price_ath: number | undefined;
 }
-
-export default Price;
